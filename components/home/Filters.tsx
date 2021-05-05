d518f9a2e2deb4e4474c4a6c38c0b677c6c034d3
epickaowca <owcaofficial@yahoo.com>
2021-05-05 20:18:12 +0200

    filters

---
+++ components/home/Filters.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.form`
+display: flex;
+flex-direction: column;
+margin: auto;
+max-width: 280px;
+& > input, & > select{
+    background-color: ${p=>p.theme.colors.backgroundColor};
+    border: 1px solid #494E6E;
+    padding: 15px;
+    color: white;
+    height: 50px;
+}
+& > select{
+    margin-top: 25px;
+}
+${p=>p.theme.media.tablet}{
+    flex-direction: row;
+    max-width: 400px;
+    & > input{
+        width: 250px;
+    }
+    & > select{
+        width: 125px;
+        margin-top: 0px;
+        margin-left: 25px;
+    }
+}
+`
+
+const Filters = () => {
+    return (
+        <Wrapper>
+            <input placeholder='name' type='text' />
+            <select>
+                <option value='all'>gender</option>
+                <option value='male'>male</option>
+                <option value='female'>female</option>
+            </select>
+        </Wrapper>
+    )
+}
+
+export default Filters
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import styled from 'styled-components'
 import CharacterList from '../components/home/CharacterList'
 import Header from '../elements/Header'
+import Filters from '../components/home/Filters'
 
 const Wrapper = styled.div`
-
+  padding: 25px;
 `
 
 const Home:React.FC = ()=> {
   return (
     <Wrapper>
-      <Header />
+      <Filters />
+      {/* <Header /> */}
       {/* <CharacterList /> */}
     </Wrapper>
     )
